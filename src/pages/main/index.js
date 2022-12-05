0973189d30393c51f021e296c4adf07946ee76a3
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2022-12-05 20:17:20 +0300

    feat: cart add

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,12 +2,16 @@ import "./style.scss";
 //import Api from "../../components/Api";
 //import { fetchParams } from "../../utils/variables";
 import logoImageSrc from "../../images/logo.svg";
+import cartLogoSrc from "../../images/cart.svg";
 import intoImageSrc from "../../images/book-image.png";
 import cardDataJSON from "../../utils/data.json";
 
 console.log("cardDataJSON", cardDataJSON);
 
 let book = null;
+//let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cart = [];
+console.log(cart);
 
 //const api = new Api(fetchParams);
 
@@ -17,30 +21,39 @@ const documentFragment = new DocumentFragment();
 const body = document.querySelector(".page");
 
 // -- HEADER--
-const header = document.createElement("header");
-const headerContainer = document.createElement("div");
-const logo = document.createElement("h1");
-const logoLink = document.createElement("a");
-const logoImg = document.createElement("img");
-const logoText = document.createElement("p");
+const renderHeader = () => {
+  const header = document.createElement("header");
+  const headerContainer = document.createElement("div");
+  const logo = document.createElement("h1");
+  const logoLink = document.createElement("a");
+  const logoImg = document.createElement("img");
+  const logoText = document.createElement("p");
+  const cartLogo = document.createElement("img");
 
-header.classList.add("header");
-headerContainer.classList.add("container");
-logo.classList.add("logo");
-logoLink.classList.add("logo__link");
-logoLink.setAttribute("href", "#");
-logoLink.setAttribute("aria-label", "main page");
-logoImg.classList.add("logo__img");
-logoImg.setAttribute("src", logoImageSrc);
-logoImg.setAttribute("alt", "book logo");
-logoText.classList.add("logo__text");
-logoText.textContent = "Page";
+  header.classList.add("header");
+  headerContainer.classList.add("container");
+  logo.classList.add("logo");
+  logoLink.classList.add("logo__link");
+  logoLink.setAttribute("href", "#");
+  logoLink.setAttribute("aria-label", "main page");
+  logoImg.classList.add("logo__img");
+  logoImg.setAttribute("src", logoImageSrc);
+  logoImg.setAttribute("alt", "book logo");
+  logoText.classList.add("logo__text");
+  logoText.textContent = "Page";
+  cartLogo.setAttribute("src", cartLogoSrc);
+  cartLogo.setAttribute("alt", "cart logo");
 
-header.appendChild(headerContainer);
-headerContainer.appendChild(logo);
-logo.appendChild(logoLink);
-logoLink.appendChild(logoImg);
-logoLink.appendChild(logoText);
+  header.appendChild(headerContainer);
+  headerContainer.appendChild(logo);
+  logo.appendChild(logoLink);
+  logoLink.appendChild(logoImg);
+  logoLink.appendChild(logoText);
+  headerContainer.appendChild(cartLogo);
+  documentFragment.append(header);
+};
+
+renderHeader();
 
 // -- MAIN
 const main = document.createElement("main");
@@ -54,11 +67,15 @@ const introText = document.createElement("p");
 const bookShelfTitle = document.createElement("h2");
 const booksContainer = document.createElement("div");
 const cardsContainer = document.createElement("div");
+const sectionCart = document.createElement("section");
+const cartTitle = document.createElement("h2");
+const cartContainer = document.createElement("div");
+const cartBooksContainer = document.createElement("div");
 
 main.classList.add("main");
 // -- sectionIntro
 sectionIntro.classList.add("section", "intro");
-sectionBookShelf.classList.add("book-shelf");
+
 introTextContainer.classList.add("intro-text-container");
 introTitle.textContent = "Welcome to Pages!!!";
 introSubtitle.textContent = "Your Books From The Best Writer.";
@@ -68,10 +85,17 @@ introImg.classList.add("intro__image");
 introImg.setAttribute("src", intoImageSrc);
 introImg.setAttribute("alt", "book image");
 // --section Book-Shelf
+sectionBookShelf.classList.add("book-shelf");
 bookShelfTitle.textContent = "The Author’s Book";
 bookShelfTitle.classList.add("book-shelf__title");
 booksContainer.classList.add("book-shelf__container");
 cardsContainer.classList.add("book-shelf__cards-container");
+// --section Cart
+sectionCart.classList.add("cart");
+cartTitle.textContent = "Your Cart";
+cartTitle.classList.add("cart__title");
+cartContainer.classList.add("cart__container");
+cartBooksContainer.classList.add("cart__cards-container");
 
 // -- sectionIntro
 main.appendChild(sectionIntro);
@@ -85,6 +109,12 @@ main.appendChild(sectionBookShelf);
 sectionBookShelf.appendChild(booksContainer);
 booksContainer.appendChild(bookShelfTitle);
 booksContainer.appendChild(cardsContainer);
+// --section Cart
+main.appendChild(sectionCart);
+sectionCart.appendChild(cartContainer);
+cartContainer.appendChild(cartTitle);
+cartContainer.appendChild(cartBooksContainer);
+
 // -- FOOTER
 const footer = document.createElement("footer");
 const footerContainer = document.createElement("div");
@@ -95,7 +125,7 @@ footerContainer.classList.add("container");
 footer.appendChild(footerContainer);
 
 // --page render--
-documentFragment.append(header);
+
 documentFragment.append(main);
 documentFragment.append(footer);
 body.appendChild(documentFragment);
@@ -105,6 +135,7 @@ async function renderCards() {
   const booksContainer = document.querySelector(".book-shelf__cards-container");
 
   cardDataJSON.forEach((item, i) => {
+    item.id = i + 1;
     const card = document.createElement("div");
     card.classList.add("card");
 
@@ -154,7 +185,19 @@ async function renderCards() {
 
     const addToBagBtn = document.createElement("button");
     addToBagBtn.classList.add("card__btn");
-    addToBagBtn.textContent = "Add to bag";
+    addToBagBtn.textContent = "Add to cart";
+
+    addToBagBtn.addEventListener("click", () => {
+      console.log("cart add", cart);
+      if (cart.find((el) => el.id === item.id)) {
+        console.log("You have alredy added this book to the cart");
+        return;
+      }
+      cart.push(item);
+      localStorage.setItem("cart", JSON.stringify(cart));
+      console.log(cart);
+      renderCart();
+    });
 
     card.appendChild(cardImgWrapper);
     cardImgWrapper.appendChild(cardImg);
@@ -177,3 +220,67 @@ async function renderCards() {
 }
 
 renderCards();
+
+function renderCart() {
+  console.log("start");
+  const documentFragment = new DocumentFragment();
+  const cartBooksContainer = document.querySelector(".cart__cards-container");
+
+  if (!cart.length) {
+    cartBooksContainer.textContent = "Your cart is empty. Add some books";
+    return;
+  }
+
+  cart.forEach((item) => {
+    const cartElement = document.createElement("div");
+    cartElement.classList.add("cart__element");
+    cartElement.setAttribute("id", item.id);
+
+    const cartElImgWrapper = document.createElement("div");
+    cartElImgWrapper.classList.add("cart__img");
+    const cartImg = document.createElement("img");
+    cartImg.setAttribute("src", item.imageLink);
+    cartImg.setAttribute("alt", item.title);
+
+    const cartInfo = document.createElement("div");
+    cartInfo.classList.add("cart__info");
+    const author = document.createElement("p");
+    author.classList.add("cart__text-author");
+    author.textContent = item.author;
+    const title = document.createElement("p");
+    title.classList.add("cart__text-title");
+    title.textContent = item.title;
+    const price = document.createElement("p");
+    price.classList.add("cart__text-price");
+    price.textContent = "$" + `${item.price}`;
+
+    const delBtnWrapper = document.createElement("div");
+    delBtnWrapper.classList.add("cart_btn-wrapper");
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "x";
+
+    delBtn.addEventListener("click", () => {
+      //console.log("id", item.id);
+      //console.log("el by id", document.getElementById(item.id));
+      cartBooksContainer.removeChild(document.getElementById(item.id));
+      cart = cart.filter((el) => el.id !== item.id);
+      localStorage.setItem("cart", JSON.stringify(cart));
+      console.log("cart after del", cart);
+    });
+
+    cartElement.appendChild(delBtnWrapper);
+    delBtnWrapper.appendChild(delBtn);
+    cartElement.appendChild(cartElImgWrapper);
+    cartElImgWrapper.appendChild(cartImg);
+    cartElement.appendChild(cartInfo);
+    cartInfo.appendChild(author);
+    cartInfo.appendChild(title);
+    cartInfo.appendChild(price);
+
+    documentFragment.appendChild(cartElement);
+  });
+
+  cartBooksContainer.appendChild(documentFragment);
+}
+
+renderCart();
