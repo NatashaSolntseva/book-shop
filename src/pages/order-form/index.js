85d704ac74b724cde6e53c4b50547653311bbdcf
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2022-12-06 21:06:56 +0300

    feat: form checkbox validation add

--- src/pages/order-form/index.js
+++ src/pages/order-form/index.js
@@ -1,3 +1,24 @@
 import "./style.scss";
 
-console.log("Hello, order!!!!");
+const form = document.getElementById("order-form");
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
+const formSubmitBtn = document.getElementById("submit");
+
+if (cart.length === 0) {
+  formSubmitBtn.setAttribute("disabled", "true");
+}
+
+function checkboxValidation() {
+  const checkBoxes = form.querySelectorAll(".order__check");
+  const giftLimit = 2;
+  function check(event) {
+    let checkedBoxes = form.querySelectorAll(".order__check:checked");
+    if (checkedBoxes.length >= giftLimit + 1) return false;
+  }
+
+  for (let i = 0; i < checkBoxes.length; i++) {
+    checkBoxes[i].onclick = check;
+  }
+}
+
+checkboxValidation();
